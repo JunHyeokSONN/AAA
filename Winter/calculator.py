f8105bfe0d84aa795f263dc35b97b3d9375b4f05
JunHyeokSONN <112154466+JunHyeokSONN@users.noreply.github.com>
2023-01-26 16:57:08 +0900

    Add files via upload

---
+++ Winter/calculator.py
@@ -0,0 +1,12 @@
+## calculator module
+def plus(a,b):
+    return a+b
+def minus(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+if __name__=='__main__':
+    x = 100
